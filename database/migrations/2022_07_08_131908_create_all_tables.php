53e28ecc647fd9d17eedc41a1c2e439fc00f280b
matheuskoba <matheuskoba2021@icloud.com>
2022-07-08 10:20:51 -0300

    add migraton banco de dados

---
+++ database/migrations/2022_07_08_131908_create_all_tables.php
@@ -0,0 +1,94 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('avatar')->default('default.png');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('tipo');
+        });
+        Schema::create('userfavorites', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('id_personal');
+        });
+        Schema::create('userappointments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('id_personal');
+            $table->integer('id_service');
+            $table->datetime('ap_datetime');
+        });
+
+        Schema::create('personals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('avatar')->default('default.png');
+            $table->float('stars')->default(5);
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+        });
+        Schema::create('personalphotos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_personal');
+            $table->string('url');
+        });
+        Schema::create('personalreviews', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_personal');
+            $table->string('rate');
+        });
+        Schema::create('personalservices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_personal');
+            $table->string('name');
+            $table->float('price');
+        });
+        Schema::create('personaltestimonials', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_personal');
+            $table->string('name');
+            $table->float('rate');
+            $table->string('body');
+        });
+        Schema::create('personalavailability', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_personal');
+            $table->integer('weekday');
+            $table->text('hours');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('userfavorite');
+        Schema::dropIfExists('userappointments');
+        Schema::dropIfExists('personals');
+        Schema::dropIfExists('personalphotos');
+        Schema::dropIfExists('personalreviews');
+        Schema::dropIfExists('personalservices');
+        Schema::dropIfExists('personaltestimonials');
+        Schema::dropIfExists('personalavailability');
+    }
+};
