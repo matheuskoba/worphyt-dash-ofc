9ac15662132a613bf6d3ea0377c6e228356b4811
Raulisson <raulissonvalbuquerque@gmail.com>
2022-07-19 22:02:25 -0300

    finishing register step 3 layout

--- resources/views/registersteps/registerstep3.blade.php
+++ resources/views/registersteps/registerstep3.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Worphyt Cadastro</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/registerstepscss/step2style.css') }}" >
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/registerstepscss/step3style.css') }}" >
 
     {{-- Fontawesome --}}
     <script src="https://kit.fontawesome.com/92e90f8568.js" crossorigin="anonymous"></script>
@@ -14,14 +14,41 @@
 <body>
     <div class="main-container">
         <img class="logo" src="{{ asset('img/logo.png') }}" alt="logo">
-        <div class="contents">
             <form method="POST" action="">
-            <span>
-                <i class="fa-solid fa-brazilian-real-sign"></i>
-            </span>
-            <input type="text" name="minorprice" placeholder="Menor preço">
+                <div class="contents">
+                    <div class="inputminorprice">
+                        <span>
+                            <i class="fa-solid fa-brazilian-real-sign"></i>
+                        </span>
+                        <input type="text" name="minorprice" placeholder="Menor preço">
+                    </div>
+                    <h1>~</h1>
+                    <div class="inputmajorprice">
+                        <span>
+                            <i class="fa-solid fa-brazilian-real-sign"></i>
+                        </span>
+                        <input type="text" name="majorprice" placeholder="Maior preço">
+                    </div>
+                </div>
+                <h1>Pacotes promocionais
+                    <i class="fa-solid fa-plus"></i>
+                </h1>
+                <div class="contents">
+                    <div class="inputhourclass">
+                        <span>
+                            <i class="fa-solid fa-clock"></i>
+                        </span>
+                        <input type="text" name="hourclass" placeholder="Hora aula">
+                    </div>
+                    <div class="inputprice">
+                        <span>
+                            <i class="fa-solid fa-brazilian-real-sign"></i>
+                        </span>
+                        <input type="text" name="price" placeholder="Preço">
+                    </div>
+                    <i class="fa-solid fa-trash"></i>
+                </div>
             </form>
-        </div>
         <button> <i class="fa-solid fa-arrow-right"></i> </button>
     </div>
 </body>
