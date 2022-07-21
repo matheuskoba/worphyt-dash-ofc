10235ebe4407f29c501fa4cccb22b4791b2bf80c
Raulisson <raulissonvalbuquerque@gmail.com>
2022-07-21 11:52:14 -0300

    adjustments of functionality on register step 5 and 6

--- resources/views/registersteps/registerstep5.blade.php
+++ resources/views/registersteps/registerstep5.blade.php
@@ -1,46 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Worphyt Cadastro</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Worphyt Cadastro</title>
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/registerstepscss/step5style.css') }}" >
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/registerstepscss/step5style.css') }}" >
 
-    {{-- Fontawesome --}}
-    <script src="https://kit.fontawesome.com/92e90f8568.js" crossorigin="anonymous"></script>
-    <script src="http://code.jquery.com/jquery-1.5.2rc1.js" crossorigin="anonymous"></script>
-</head>
-<body>
-<div class="main-container">
-    <img class="logo" src="{{ asset('img/logo.png') }}" alt="logo">
-    <form method="POST" action="">
-        <div class="plus box">
-            <p>Idiomas</p>
-            <button type="button" id="add" class="plusbutton"><i class="fa-solid fa-plus"></i></button>
-        </div>
-        <div id="buildyourform">
+        {{-- Fontawesome --}}
+        <script src="https://kit.fontawesome.com/92e90f8568.js" crossorigin="anonymous"></script>
+        <script src="http://code.jquery.com/jquery-1.5.2rc1.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="main-container">
+            <img class="logo" src="{{ asset('img/logo.png') }}" alt="logo">
+            <form method="POST" action="">
+                <div class="plus box">
+                    <p>Idiomas</p>
+                    <button type="button" id="add" class="plusbutton"><i class="fa-solid fa-plus"></i></button>
+                </div>
+                <div id="buildyourform">
 
+                </div>
+                <div class="check adjust">
+                    <div class="check presential">
+                        <input type="checkbox" name="presential" id="presential">
+                        <label for="presential">Atendimento presencial</label>
+                    </div>
+                    <div class="check remote">
+                        <input type="checkbox" name="remote" id="remote">
+                        <label for="remote">Atendimento remoto</label>
+                    </div>
+                </div>
+                <button><i class="fa-solid fa-arrow-right"></i></button>
+            </form>
         </div>
-        <div class="inputlanguage">
-                <span>
-                    <i class="fa-solid fa-language"></i>
-                </span>
-            <input type="text" name="language" placeholder="Idioma">
-            <i class="fa-solid fa-trash"></i>
-        </div>
-        <div class="check">
-            <input type="checkbox" name="presential" id="presential">
-            <label for="presential">Atendimento presencial</label>
-        </div>
-        <div class="check adjust">
-            <input type="checkbox" name="remote" id="remote">
-            <label for="remote">Atendimento remoto</label>
-        </div>
-    </form>
-    <button> <i class="fa-solid fa-arrow-right"></i> </button>
-</div>
+        <script>
+            $(document).ready(function() {
+            $("#add").click(function() {
+                var lastField = $("#buildyourform div:last");
+                var intId = (lastField && lastField.length && lastField.data("idx") + 1) || 1;
+                var fieldWrapper = $("<div class=\"fieldwrapper\" id=\"field" + intId + "\"/>");
+                fieldWrapper.data("idx", intId);
+                var fName = $("<div class=\"contents\"><div class=\"inputlanguage\"><span><i class=\"fa-solid fa-language\"></i></span><input type=\"text\" name=\"langauge\" placeholder=\"Idioma\"></div></div>");
+                var removeButton = $("<button id=\"remove\"><i class=\"fa-solid fa-trash\"></i></button>");
+                removeButton.click(function() {
+                    $(this).parent().remove();
+                });
+                fieldWrapper.append(fName);
+                fieldWrapper.append(removeButton);
+                $("#buildyourform").append(fieldWrapper);
+            });
+        });
+        </script>
 
-</body>
+    </body>
 </html>
--- resources/views/registersteps/registerstep6.blade.php
+++ resources/views/registersteps/registerstep6.blade.php
@@ -1,35 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Worphyt Cadastro</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Worphyt Cadastro</title>
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/registerstepscss/step6style.css') }}" >
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/registerstepscss/step6style.css') }}" >
 
-    {{-- Fontawesome --}}
-    <script src="https://kit.fontawesome.com/92e90f8568.js" crossorigin="anonymous"></script>
+        {{-- Fontawesome --}}
+        <script src="https://kit.fontawesome.com/92e90f8568.js" crossorigin="anonymous"></script>
+        {{-- JQUERY --}}
+        <script src="http://code.jquery.com/jquery-1.5.2rc1.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="main-container">
+            <img class="logo" src="{{ asset('img/logo.png') }}" alt="logo">
+            <form method="POST" action="">
+                <div class="box">
+                    <p>Academias</p>
+                    <button type="button" id="add"><i class="fa-solid fa-plus"></i></button>
+                </div>
+                <div id="buildyourform">
 
-</head>
-<body>
-<div class="main-container">
-    <img class="logo" src="{{ asset('img/logo.png') }}" alt="logo">
-    <div>
-        <p>Academias</p>
-        <i class="fa-solid fa-plus"></i>
-    </div>
-    <form method="POST" action="">
-        <div class="inputcref">
-                <span>
-                    <i class="fa-solid fa-dumbbell"></i>
-                </span>
-            <input type="text" name="gym" placeholder="Nome da academia">
-            <i class="fa-solid fa-trash"></i>
+                </div>
+                <button> <i class="fa-solid fa-arrow-right"></i> </button>
+            </form>
         </div>
-    </form>
-    <button> <i class="fa-solid fa-arrow-right"></i> </button>
-</div>
+    <script>
+        $(document).ready(function() {
+        $("#add").click(function() {
+        var lastField = $("#buildyourform div:last");
+        var intId = (lastField && lastField.length && lastField.data("idx") + 1) || 1;
+        var fieldWrapper = $("<div class=\"fieldwrapper\" id=\"field" + intId + "\"/>");
+        fieldWrapper.data("idx", intId);
+        var fName = $("<div class=\"contents\"><div class=\"inputcref box-input\"><span><i class=\"fa-solid fa-dumbbell\"></i></span><input type=\"text\" name=\"gym\" placeholder=\"Nome da Academia\"></div></div>");
+        var removeButton = $("<button id=\"remove\"><i class=\"fa-solid fa-trash\"></i></button>");
+        removeButton.click(function() {
+            $(this).parent().remove();
+        });
+        fieldWrapper.append(fName);
+        fieldWrapper.append(removeButton);
+        $("#buildyourform").append(fieldWrapper);
+    });
+});
+    </script>
 
-</body>
+    </body>
 </html>
